234cafc4935a09d203344f79d576fb5d244b5b20
o-thiago <thiagomacedos@disroot.org>
2025-01-14 11:15:11 -0300

    Adds a basic navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,62 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Drawer,
+  DrawerHeader,
+  DrawerContent,
+  DrawerTrigger,
+  DrawerTitle,
+} from "@/components/ui/drawer";
+import { Menu } from "lucide-react";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+
+function NavContent() {
+  return (
+    <>
+      <Button variant="link">
+        <Link href="/login">Login</Link>
+      </Button>
+      <Button variant="secondary">
+        <Link href="/login">Sign up</Link>
+      </Button>
+    </>
+  );
+}
+
+export function Navbar() {
+  return (
+    <header className="w-full border-b bg-secondary-foreground">
+      <div className="flex items-center justify-between p-4">
+        <Link href="/" className="flex items-center">
+          <span className="hidden font-bold sm:inline-block text-secondary">
+            LastCommunity
+          </span>
+        </Link>
+        <div className="justify-between space-x-4 hidden md:block">
+          <NavContent />
+        </div>
+        <div className="md:hidden">
+          <Drawer direction="left">
+            <DrawerTrigger asChild>
+              <Button variant="ghost">
+                <Menu className="text-secondary" />
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent className="h-full w-10/12 rounded-none">
+              <VisuallyHidden>
+                <DrawerHeader>
+                  <VisuallyHidden>
+                    <DrawerTitle>Navigation drawer</DrawerTitle>
+                  </VisuallyHidden>
+                </DrawerHeader>
+              </VisuallyHidden>
+              <div className="flex flex-col">
+                <NavContent />
+              </div>
+            </DrawerContent>
+          </Drawer>
+        </div>
+      </div>
+    </header>
+  );
+}
