70433a94c0af999a53a6a219088bc6af3d60df67
o-thiago <thiagomacedos@disroot.org>
2025-01-14 13:23:58 -0300

    Sets authentication cookie

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,8 +1,29 @@
 // app/api/[[...slugs]]/route.ts
 import { Elysia, t } from "elysia";
+import { NextResponse } from "next/server";
+
+const getFullUrl = (subRoute: string = "") =>
+  `http://localhost:3000/${subRoute}`;
 
 const app = new Elysia({ prefix: "/api" })
-  .get("/", () => "hello Next")
+  .get(
+    "/auth",
+    ({ query, cookie: { token: storedCookie } }) => {
+      if (query.token) {
+        storedCookie.value = query.token;
+        storedCookie.secure = true;
+        storedCookie.sameSite = true;
+        storedCookie.httpOnly = true;
+      }
+
+      return NextResponse.redirect(getFullUrl());
+    },
+    {
+      query: t.Object({
+        token: t.String(),
+      }),
+    },
+  )
   .post("/", ({ body }) => body, {
     body: t.Object({
       name: t.String(),
