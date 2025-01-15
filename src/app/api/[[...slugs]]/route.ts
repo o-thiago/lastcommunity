0d6b0ed385d63103cb09d2bc7a6ed13629a8cbff
o-thiago <thiagomacedos@disroot.org>
2025-01-15 07:59:14 -0300

    Better error handling

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,10 +1,18 @@
-// app/api/[[...slugs]]/route.ts
 import { Elysia } from "elysia";
 import { elysiaAuth } from "./auth";
+import { simpleResponseLayer } from "./utils";
 
 const apiRoute = new Elysia({ prefix: "/api" }).use(elysiaAuth);
-
-const rootRoute = new Elysia().use(apiRoute);
+const rootRoute = new Elysia()
+  .use(simpleResponseLayer)
+  .onError(({ error, code, responses }) => {
+    console.error({ error, code });
+    return {
+      code,
+      error: responses.INTERNAL_ERROR,
+    };
+  })
+  .use(apiRoute);
 
 export const GET = rootRoute.handle;
 export const POST = rootRoute.handle;
